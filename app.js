3168db0361436e8fb032ca62dec1d6d280b331f0
Ogoh Cyril <cyrilogoh@gmail.com>
2022-04-09 19:08:18 +0100

    added DATABASE

--- app.js
+++ app.js
@@ -3,17 +3,54 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-//
+//data base and  seesion
 const mongoose = require('mongoose');
 var passport = require('passport');
 var session = require('express-session');
 var MongoDBStore = require('connect-mongodb-session')(session);
+const { database } = require('./database/config')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var ussdRouter = require('./routes/ussd');
 
 var app = express();
+// Database intergration
+
+var db = database.cloud // change data 
+// mongodb connector 
+mongoose.connect(db, {
+  useNewUrlParser: true,useUnifiedTopology:true
+},(error)=>{
+  if(error){console.log(error.message)}else{
+    console.log("database connected Successfully");
+  }
+});
+
+var store = new MongoDBStore({
+  uri: db,
+  collection: 'mySessions'
+},
+function(error) {
+  if(error){
+    console.log(error);
+  }
+});
+
+
+store.on('error', function(error) {
+  console.log("Error occurs no storage of session on db");
+});
+
+app.use(session({
+  secret: "ilovecoding",//process.env.SECRET,
+  resave: false,
+  saveUninitialized: true,
+  store: store, // uncomment this for mongo storage
+  cookie: {
+      maxAge: 1000 * 60 * 60 * 24 // Equals 1 day (1 day * 24 hr/1 day * 60 min/1 hr * 60 sec/1 min * 1000 ms / 1 sec)
+  }
+}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
---
+++ database/config.js
@@ -0,0 +1,4 @@
+exports.database = {
+    local:"mongodb://localhost:27017/nazo",
+    cloud:  "mongodb+srv://admin:Admin100@cluster0.uqax3.mongodb.net/nazo?retryWrites=true&w=majority"
+}
